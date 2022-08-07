195844d1128d0aec92c00c255b93a613e8bedca6
yusufbaltamegacode <98096643+yusufbaltamegacode@users.noreply.github.com>
2022-08-07 10:01:13 +0300

    Initial Commit

---
+++ coredataexample/coredataexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  coredataexampleApp.swift
+//  coredataexample
+//
+//  Created by Yusuf Balta on 7.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct coredataexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
